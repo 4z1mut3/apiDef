7223eba76c3f1b0ba25ba457729161579d557687
paulo_andr3 <paulo.soliveira@valid.com>
2022-12-18 14:07:53 -0300

    inicialização

---
+++ Repositories/Implementations/UserRepository.cs
@@ -0,0 +1,48 @@
+﻿using Dapper;
+using MinimalJwt.Models;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace MinimalJwt.Repositories
+{
+    public class UserRepository
+    {
+        private static IConfiguration _configuration;
+        public readonly List<User> _listaDeUsuarios;
+
+        public UserRepository(IConfiguration configuration, List<User> listaDeUsuarios)
+        {
+            _configuration = configuration;
+            _listaDeUsuarios = listaDeUsuarios;
+        }
+
+        public static List<User> Users = ListarUsuarios();
+        
+        public static List<User> ListarUsuarios()
+        {
+            var usuarios = new List<User>();
+            
+            try
+            {                                            
+                using (IDbConnection db = new MySqlConnection(_configuration.GetConnectionString("ConnectionStringMysql")))
+                {
+                    db.Open();
+                    Users = db.Query<User>(@" SELECT IdUser, 
+                                                    Username, 
+                                                    Password, 
+                                                    EmailAddress, GivenName, `Role`
+                                                    FROM testedapper.`user`; ").ToList();
+                    
+                    return Users;
+                }
+                
+            }
+            catch (MySqlException ex)
+            {
+                return Users;
+            }
+            
+            
+        }
+    }
+}
---
+++ Services/Implementations/UserService.cs
@@ -0,0 +1,45 @@
+﻿using Dapper;
+using MinimalJwt.Models;
+using MinimalJwt.Repositories;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace MinimalJwt.Services
+{
+    public class UserService : IUserService
+    {
+        //private readonly MySqlConnection _connection;
+        private readonly IConfiguration _configuration;
+
+        public UserService(IConfiguration configuration) 
+        {
+            _configuration = configuration;
+        }
+        public User Get(UserLogin userLogin)
+        {
+            User user = listarUsuarios().FirstOrDefault(o => o.Username.Equals(userLogin.Username, StringComparison.OrdinalIgnoreCase) && o.Password.Equals(userLogin.Password));
+       
+            return user;
+        }
+
+        public List<User> listarUsuarios() 
+        {
+            List<User> Users = new List<User>();
+            try
+            {
+                using (IDbConnection db = new MySqlConnection(_configuration.GetConnectionString("conn")))
+                {
+                    db.Open();
+                    Users = db.Query<User>(@" SELECT * FROM testedapper.`user`; ").ToList();
+
+
+                }
+                return Users;
+            }
+            catch (MySqlException ex)
+            {
+                return Users;
+            }
+        }
+    }
+}
